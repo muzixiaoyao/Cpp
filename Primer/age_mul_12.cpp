75cb2abeb77104775cde76d4d1dde98a0c150ef5
xcracker <wsxiaoyao@live.com>
2015-10-01 00:53:49 +0800

    Update and rename old_mul_12.cpp to age_mul_12.cpp

--- Primer/age_mul_12.cpp
+++ Primer/age_mul_12.cpp
@@ -1,22 +1,24 @@
+//第二天
+//输入年龄，显示该年龄包含了多少个月份
 #include "stdafx.h"
 #include <iostream>
 #include <cmath>
 
 using namespace std;
-void old_mul_12(void);
+void age_mul_12(void);
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-	old_mul_12();
+	age_mul_12();
 
 	cin.get();
 	cin.get();
 	return 0;
 }
 
-void old_mul_12(void)
+void age_mul_12(void)
 {
-	cout << "Please input your old: " << endl;
+	cout << "Please input your age: " << endl;
 	int old;
 	cin >> old;
 	cout << "You month include " << old * 12 << " months!" << endl;
